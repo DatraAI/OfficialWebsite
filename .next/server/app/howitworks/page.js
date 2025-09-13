e5f618c536e0f3527ab59e6460bfe5419756e97a
tigercosmos <acliu@datra.ai>
2025-09-14 02:36:19 +0900

    upload

--- .next/server/app/howitworks/page.js
+++ .next/server/app/howitworks/page.js
@@ -647,7 +647,7 @@ const __default__ = proxy.default;
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [160,967], () => (__webpack_exec__(4134)));
+var __webpack_exports__ = __webpack_require__.X(0, [160,378], () => (__webpack_exec__(4134)));
 module.exports = __webpack_exports__;
 
 })();
